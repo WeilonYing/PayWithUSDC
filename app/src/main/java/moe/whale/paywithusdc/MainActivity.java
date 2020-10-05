80eac83928e3ef5d18f2c028d82ce30f4fd1fe06
WeilonYing <weilony@hotmail.com>
2020-10-04 17:52:04 -0700

    Add intro screen

--- app/src/main/java/moe/whale/paywithusdc/MainActivity.java
+++ app/src/main/java/moe/whale/paywithusdc/MainActivity.java
@@ -3,6 +3,11 @@ package moe.whale.paywithusdc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.web3j.crypto.WalletUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButtons();
+        // WalletUtils.generateNewWalletFile()
+    }
+
+    private void initButtons() {
+        Button newWallet = findViewById(R.id.activity_main_button_new);
+        newWallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), getString(R.string.common_new), Toast.LENGTH_SHORT).show();
+                System.out.println("asfasdfdsfdsfsdddfdfdf;sdka");
+            }
+        });
+
+        Button loadWallet = findViewById(R.id.activity_main_button_load);
+        loadWallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), getString(R.string.common_load), Toast.LENGTH_SHORT).show();
+                System.out.println("salfjasfjasdjfl;sdka");
+            }
+        });
     }
 }
