494807a481e72c8ff6c9996bb96d895d2d7da6d5
WeilonYing <weilony@hotmail.com>
2020-10-04 18:29:43 -0700

    Stub out new wallet activity

---
+++ app/src/main/java/moe/whale/paywithusdc/NewWalletActivity.java
@@ -0,0 +1,15 @@
+package moe.whale.paywithusdc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewWalletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.new_wallet_activity_title);
+        setContentView(R.layout.activity_new_wallet);
+    }
+}
