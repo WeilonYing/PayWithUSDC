da599bca3b213db5fdd5ba978d3869fdfa8c7701
WeilonYing <weilony@hotmail.com>
2020-10-04 19:27:17 -0700

    Add file permissions and debug wallet generation

--- app/src/main/java/moe/whale/paywithusdc/MainActivity.java
+++ app/src/main/java/moe/whale/paywithusdc/MainActivity.java
@@ -1,20 +1,42 @@
 package moe.whale.paywithusdc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.web3j.crypto.CipherException;
 import org.web3j.crypto.WalletUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Provider;
+import java.security.Security;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupBouncyCastle();
         setContentView(R.layout.activity_main);
         initButtons();
         // WalletUtils.generateNewWalletFile()
@@ -33,9 +55,7 @@ public class MainActivity extends AppCompatActivity {
         loadWallet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast
-                    .makeText(getApplicationContext(), getString(R.string.common_load), Toast.LENGTH_SHORT)
-                    .show();
+                startLoadWalletFilePicker();
             }
         });
     }
@@ -43,5 +63,71 @@ public class MainActivity extends AppCompatActivity {
     private void startNewWalletActivity() {
         Intent i = new Intent(this, NewWalletActivity.class);
         startActivity(i);
+    }
+
+    private void startLoadWalletFilePicker() {
+        DEBUG_createNewWalletFile();
+        Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        i.addCategory(Intent.CATEGORY_OPENABLE);
+        i.setType("*/*");
+
+        startActivityForResult(i, 2);
+    }
+
+    private void setupBouncyCastle() {
+        final Provider provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME);
+        if (provider == null) {
+            // Web3j will set up the provider lazily when it's first used.
+            return;
+        }
+        if (provider.getClass().equals(BouncyCastleProvider.class)) {
+            // BC with same package name, shouldn't happen in real life.
+            return;
+        }
+        // Android registers its own BC provider. As it might be outdated and might not include
+        // all needed ciphers, we substitute it with a known BC bundled in the app.
+        // Android's BC has its package rewritten to "com.android.org.bouncycastle" and because
+        // of that it's possible to have another BC implementation loaded in VM.
+        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
+        Security.insertProviderAt(new BouncyCastleProvider(), 1);
+    }
+
+    private void DEBUG_createNewWalletFile() {
+        verifyStoragePermissions(this);
+        try {
+            WalletUtils.generateNewWalletFile("password", new File(Environment.getExternalStorageDirectory().getPath() + "/Download"));
+        } catch (CipherException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 }
